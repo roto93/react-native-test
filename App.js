64a333becf023938a9b9c93e2b162757cf5d9388
roto93 <roto93@yahoo.com.tw>
2021-05-27 12:31:07 +0800

    5/27 try accordion

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import Collapsible from 'react-native-collapsible'
+import Accordion from 'react-native-collapsible/Accordion'
 
 const MOCK = [
   {
@@ -92,7 +93,9 @@ const MOCK = [
 
 
 export default function App() {
+  const [toAccordionPage, setToAccordionPage] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(true);
+  const [activeSections, setActiveSections] = useState([]);
 
   const RenderItem = ({ item, index }) => {
 
@@ -106,22 +109,40 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title={'open'} onPress={() => { setIsCollapsed(!isCollapsed) }} />
-      <View>
+      <Button title={'next'} onPress={() => { setToAccordionPage(!toAccordionPage) }} />
+      <View style={{ height: 40 }} />
+      {!toAccordionPage && <View style={{ width: 250 }}>
+
         <Text>This is a collapsible.</Text>
+        <Button title={'open'} onPress={() => { setIsCollapsed(!isCollapsed) }} />
+        <Collapsible
+          collapsed={isCollapsed} style={{ width: 250 }}
+        >
+          <View style={{ width: '100%', height: 250, backgroundColor: '#e1e1e1' }}>
+            <FlatList
+              data={MOCK}
+              renderItem={({ item, index }) => <RenderItem item={item} index={index} />}
+              keyExtractor={(_, i) => i.toString()}
+              style={{ width: '100%', }}
+            />
+          </View>
+        </Collapsible>
       </View>
-      <Collapsible
-        collapsed={isCollapsed} style={{ width: 250 }}
-      >
-        <View style={{ width: '100%', height: 250, backgroundColor: '#e1e1e1' }}>
-          <FlatList
-            data={MOCK}
-            renderItem={({ item, index }) => <RenderItem item={item} index={index} />}
-            keyExtractor={(_, i) => i.toString()}
-            style={{ width: '100%', }}
+      }
+      {toAccordionPage &&
+        <View style={{ width: 250 }}>
+          <Text>This is an accordion.</Text>
+          <Accordion
+            sections={MOCK}
+            activeSections={activeSections}
+            renderSectionTitle={(i, j, k, l) => { return (<Text style={{ marginTop: 8 }}>{j + 1}</Text>) }}
+            renderHeader={() => (<View style={{ width: 250, height: 40, backgroundColor: 'gray', }}></View>)}
+            renderContent={() => (<View style={{ width: 250, height: 100, backgroundColor: '#fff', borderWidth: 1, }}></View>)}
+            onChange={(activeSectionArray) => { setActiveSections(activeSectionArray) }}
+            touchableComponent={TouchableOpacity}
           />
         </View>
-      </Collapsible>
+      }
       <StatusBar style="auto" />
     </View>
   );
