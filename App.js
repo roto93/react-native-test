10db8a6151face0cd2b9a3c5f6d6047fe0019b99
roto93 <roto93@yahoo.com.tw>
2021-06-10 14:06:04 +0800

    6/10 try Expo ImagePicker and FileSystem.

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system'
 
 export default function App() {
+  const [imageUri, setImageUri] = useState(null);
+  const openImagePicker = async () => {
+    try {
+      const response = await ImagePicker.launchImageLibraryAsync({ allowsEditing: true, quality: 0.5 })
+      console.log(response)
+      if (response.uri) {
+        setImageUri(response.uri)
+        await FileSystem.copyAsync({ from: response.uri, to: FileSystem.documentDirectory + 'test/test.jpg' })
+        console.log(FileSystem.documentDirectory)
+      }
+    } catch (e) { alert(e) }
+  }
+  const openCamera = async () => {
+    const response = await ImagePicker.launchCameraAsync()
+    console.log(response)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="open image" onPress={openImagePicker} />
+      {imageUri && <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />}
+      <Button title="open camera" onPress={openCamera} />
     </View>
   );
 }
